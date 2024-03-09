f54610bdc2c93eafb414f8244579547f0fc42a4e
TitarAn <an.titar@eos-ts.com>
2024-03-09 21:14:21 +0100

    init custom oh-my-zsh config

---
+++ my-aliases.zsh
@@ -0,0 +1,6 @@
+alias st='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python3 -'
+alias c="clear;"
+alias h="history"
+alias hg="history | grep"
+alias editAliases="vi ~/.oh-my-zsh/custom/my-aliases.zsh"
+alias editConfig="vi ~/.zshrc"
